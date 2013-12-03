1f9ef81c323e4d8be41b6ae5d285b8d4ca57e464
Ben Pringle <ben.pringle@gmail.com>
2013-12-02 19:00:43 -0500

    Create struct representations for messages

--- protocol.h
+++ protocol.h
@@ -9,6 +9,7 @@
 #define FIELD_SIZE  128
 #define FIELDS 8
 
+#define INVALID_MESSAGE_TYPE -2
 #define NULL_MESSAGE_ID 0
 #define ERROR_MESSAGE_ID -1
 #define LOGIN_REQUEST_ID 1
@@ -26,44 +27,329 @@
 #define AUTH_FAILURE 3
 #define INSUFFICIENT_FUNDS 4
 
+int valid_message_types[] = {0, -1, 1, 2, 3, 4, 51, 52, 53, 54};
+int valid_message_types_size = 10;
+int valid_error_types[] = {0, 1, 2, 3, 4};
+int valid_error_types_size = 5;
+
 typedef char Packet [PACKET_SIZE];
 typedef char DataField [FIELD_SIZE];
 typedef const char *Field;
 
-bool validate_message_type(Packet const &packet);
+struct error_message_t {
+    int error_code;
+    std::string error_message;
+    DataField atm_nonce;
+    DataField bank_nonce;
+};
 
+struct nonce_request_t {
+    DataField atm_nonce;
+};
+struct nonce_response_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+};
+
+struct login_request_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+    std::string username;
+    std::string card;
+    std::string pin;
+};
+struct login_response_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+    DataField auth_token;
+};
+
+struct balance_request_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+    DataField auth_token;
+};
+struct balance_response_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+    unsigned int balance;
+};
+
+struct withdraw_request_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+    DataField auth_token;
+    unsigned int amount;
+};
+struct withdraw_response_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+};
+
+struct transfer_request_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+    DataField auth_token;
+    std::string destination;
+    unsigned int amount;
+};
+struct transfer_response_t {
+    DataField atm_nonce;
+    DataField bank_nonce;
+};
+
+// PUBLIC -- encoder functions
+bool encode_null_message(Packet &packet);
+bool encode_error_message(Packet &packet, const error_message_t &msg);
+bool encode_login_request(Packet &packet, const login_request_t &msg);
+bool encode_balance_request(Packet &packet, const balance_request_t &msg);
+bool encode_withdraw_request(Packet &packet, const withdraw_request_t &msg);
+bool encode_transfer_request(Packet &packet, const transfer_request_t &msg);
+bool encode_login_response(Packet &packet, const login_response_t &msg);
+bool encode_balance_response(Packet &packet, const balance_response_t &msg);
+bool encode_withdraw_response(Packet &packet, const withdraw_response_t &msg);
+bool encode_transfer_response(Packet &packet, const transfer_response_t &msg);
+
+// PUBLIC -- message type checker
+int get_message_type(Packet const &packet);
+
+// PUBLIC -- decoder functions
+bool decode_error_message(Packet const &packet, error_message_t &msg);
+bool decode_login_request(Packet const &packet, login_request_t &msg);
+bool decode_balance_request(Packet const &packet, balance_request_t &msg);
+bool decode_withdraw_request(Packet const &packet, withdraw_request_t &msg);
+bool decode_transfer_request(Packet const &packet, transfer_request_t &msg);
+bool decode_login_response(Packet const &packet, login_response_t &msg);
+bool decode_balance_response(Packet const &packet, balance_response_t &msg);
+bool decode_withdraw_response(Packet const &packet, withdraw_response_t &msg);
+bool decode_transfer_response(Packet const &packet, transfer_response_t &msg);
+
+// PUBLIC -- encryption wrappers
+void encrypt_packet(Packet &packet);
+void decrypt_packet(Packet &packet);
+void randomize(char *destination, size_t amount);
+
+/********************
+ * HELPER FUNCTIONS *
+ ********************/
+
+bool validate_message_type(Packet const &packet, int expected_type);
+void randomize_remaining_fields(Packet &packet, size_t start);
 bool get_int_field(Packet const &packet, int field, int &result);
+bool get_unsigned_int_field(Packet const &packet, int field, unsigned int &result);
 bool get_str_field(Packet const &packet, int field, std::string &result);
 bool get_dat_field(Packet const &packet, int field, DataField &result);
-
 bool set_int_field(Packet &packet, int field, int value);
+bool set_unsigned_int_field(Packet &packet, int field, unsigned int value);
 bool set_str_field(Packet &packet, int field, std::string const &value);
 bool set_dat_field(Packet &packet, int field, DataField const &value);
-
 char *get_field(Packet &packet, int field);
 const char *get_field(Packet const &packet, int field);
-
 bool int_to_field(int i, char *field);
 bool field_to_int(const char *field, int &i);
 bool field_to_unsigned_int(const char *field, unsigned int &i);
-
 bool string_to_field(std::string const &string, char *field);
 bool field_to_string(const char *field, std::string &string);
 
-// Encryption wrappers
-void encrypt_packet(Packet const &plaintext, Packet &ciphertext);
-void decrypt_packet(Packet const &ciphertext, Packet &plaintext);
-void randomize(char *destination, size_t amount);
+int get_message_type(Packet const &packet) {
+    int message_type;
+    if(!get_int_field(packet, 0, message_type)) { return INVALID_MESSAGE_TYPE; }
+    for(int i = 0; i < valid_message_types_size; i++) {
+        if(message_type == valid_message_types[i]) {
+            return message_type;
+        }
+    }
+    return INVALID_MESSAGE_TYPE;
+}
 
-bool validate_message_type(Packet const &packet) {
+bool encode_null_message(Packet &packet) {
+    if(!set_int_field(packet, 0, NULL_MESSAGE_ID)) { return false; }
+    randomize_remaining_fields(packet, 1);
+    return true;
+}
+
+bool encode_error_message(Packet &packet, const error_message_t &msg) {
+    if(!set_int_field(packet, 0, ERROR_MESSAGE_ID)) { return false; }
+    if(!set_int_field(packet, 1, msg.error_code)) { return false; }
+    if(!set_str_field(packet, 2, msg.error_message)) { return false; }
+    if(!set_dat_field(packet, 3, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 4, msg.bank_nonce)) { return false; }
+    randomize_remaining_fields(packet, 5);
+    return true;
+}
+
+bool decode_error_message(Packet const &packet, error_message_t &msg) {
+    if(!validate_message_type(packet, ERROR_MESSAGE_ID)) { return false; }
+    if(!get_int_field(packet, 1, msg.error_code)) { return false; }
+    if(!get_str_field(packet, 2, msg.error_message)) { return false; }
+    if(!get_dat_field(packet, 3, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 4, msg.bank_nonce)) { return false; }
+    return true;
+}
+
+bool encode_login_request(Packet &packet, const login_request_t &msg) {
+    if(!set_int_field(packet, 0, LOGIN_REQUEST_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!set_str_field(packet, 3, msg.username)) { return false; }
+    if(!set_str_field(packet, 4, msg.card)) { return false; }
+    if(!set_str_field(packet, 5, msg.pin)) { return false; }
+    randomize_remaining_fields(packet, 6);
+    return true;
+}
+
+bool decode_login_request(Packet const &packet, login_request_t &msg) {
+    if(!validate_message_type(packet, LOGIN_REQUEST_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!get_str_field(packet, 3, msg.username)) { return false; }
+    if(!get_str_field(packet, 4, msg.card)) { return false; }
+    if(!get_str_field(packet, 5, msg.pin)) { return false; }
+    return true;
+}
+
+bool encode_balance_request(Packet &packet, const balance_request_t &msg) {
+    if(!set_int_field(packet, 0, BALANCE_REQUEST_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!set_dat_field(packet, 3, msg.auth_token)) { return false; }
+    randomize_remaining_fields(packet, 4);
+    return true;
+}
+
+bool decode_balance_request(Packet const &packet, balance_request_t &msg) {
+    if(!validate_message_type(packet, BALANCE_REQUEST_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!get_dat_field(packet, 3, msg.auth_token)) { return false; }
+    return true;
+}
+
+bool encode_withdraw_request(Packet &packet, const withdraw_request_t &msg) {
+    if(!set_int_field(packet, 0, WITHDRAW_REQUEST_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!set_dat_field(packet, 3, msg.auth_token)) { return false; }
+    if(!set_unsigned_int_field(packet, 4, msg.amount)) { return false; }
+    randomize_remaining_fields(packet, 5);
+    return true;
+}
+
+bool decode_withdraw_request(Packet const &packet, withdraw_request_t &msg) {
+    if(!validate_message_type(packet, WITHDRAW_REQUEST_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!get_dat_field(packet, 3, msg.auth_token)) { return false; }
+    if(!get_unsigned_int_field(packet, 4, msg.amount)) { return false; }
+    return true;
+}
+
+bool encode_transfer_request(Packet &packet, const transfer_request_t &msg) {
+    if(!set_int_field(packet, 0, TRANSFER_REQUEST_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!set_dat_field(packet, 3, msg.auth_token)) { return false; }
+    if(!set_str_field(packet, 4, msg.destination)) { return false; }
+    if(!set_unsigned_int_field(packet, 5, msg.amount)) { return false; }
+    randomize_remaining_fields(packet, 6);
+    return true;
+}
+
+bool decode_transfer_request(Packet const &packet, transfer_request_t &msg) {
+    if(!validate_message_type(packet, TRANSFER_REQUEST_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!get_dat_field(packet, 3, msg.auth_token)) { return false; }
+    if(!get_str_field(packet, 4, msg.destination)) { return false; }
+    if(!get_unsigned_int_field(packet, 5, msg.amount)) { return false; }
+    return true;
+}
+
+bool encode_login_response(Packet &packet, const login_response_t &msg) {
+    if(!set_int_field(packet, 0, LOGIN_RESPONSE_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!set_dat_field(packet, 3, msg.auth_token)) { return false; }
+    randomize_remaining_fields(packet, 4);
+    return true;
+}
+
+bool decode_login_response(Packet const &packet, login_response_t &msg) {
+    if(!validate_message_type(packet, LOGIN_RESPONSE_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!get_dat_field(packet, 3, msg.auth_token)) { return false; }
+    return true;
+}
+
+bool encode_balance_response(Packet &packet, const balance_response_t &msg) {
+    if(!set_int_field(packet, 0, BALANCE_RESPONSE_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!set_unsigned_int_field(packet, 3, msg.balance)) { return false; }
+    randomize_remaining_fields(packet, 4);
+    return true;
+}
+
+bool decode_balance_response(Packet const &packet, balance_response_t &msg) {
+    if(!validate_message_type(packet, BALANCE_RESPONSE_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    if(!get_unsigned_int_field(packet, 3, msg.balance)) { return false; }
+    return true;
+}
+
+bool encode_withdraw_response(Packet &packet, const withdraw_response_t &msg) {
+    if(!set_int_field(packet, 0, WITHDRAW_RESPONSE_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    randomize_remaining_fields(packet, 3);
+    return true;
+}
+
+bool decode_withdraw_response(Packet const &packet, withdraw_response_t &msg) {
+    if(!validate_message_type(packet, WITHDRAW_RESPONSE_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    return true;
+}
+
+bool encode_transfer_response(Packet &packet, const transfer_response_t &msg) {
+    if(!set_int_field(packet, 0, TRANSFER_RESPONSE_ID)) { return false; }
+    if(!set_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!set_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    randomize_remaining_fields(packet, 3);
+    return true;
+}
+
+bool decode_transfer_response(Packet const &packet, transfer_response_t &msg) {
+    if(!validate_message_type(packet, TRANSFER_RESPONSE_ID)) { return false; }
+    if(!get_dat_field(packet, 1, msg.atm_nonce)) { return false; }
+    if(!get_dat_field(packet, 2, msg.bank_nonce)) { return false; }
+    return true;
+}
+
+bool validate_message_type(Packet const &packet, int expected_type) {
     int message_type;
     if(!get_int_field(packet, 0, message_type)) { return false; }
-    return true;
+    return message_type == expected_type;
+}
+
+void randomize_remaining_fields(Packet &packet, size_t start) {
+    char *start_ptr = packet + start * FIELD_SIZE;
+    size_t padding_size = (FIELDS - start) * FIELD_SIZE;
+    randomize(start_ptr, padding_size);
 }
 
 bool get_int_field(Packet const &packet, int field, int &result) {
     const char *field_ptr = get_field(packet, field);
     if(!field_to_int(field_ptr, result)) { return false; }
+    return true;
+}
+
+bool get_unsigned_int_field(Packet const &packet, int field, unsigned int &result) {
+    const char *field_ptr = get_field(packet, field);
+    if(!field_to_unsigned_int(field_ptr, result)) { return false; }
     return true;
 }
 
@@ -80,6 +366,12 @@ bool get_dat_field(Packet const &packet, int field, DataField &result) {
 }
 
 bool set_int_field(Packet &packet, int field, int value) {
+    char *field_ptr = get_field(packet, field);
+    if(!int_to_field(value, field_ptr)) { return false; }
+    return true;
+}
+
+bool set_unsigned_int_field(Packet &packet, int field, unsigned int value) {
     char *field_ptr = get_field(packet, field);
     if(!int_to_field(value, field_ptr)) { return false; }
     return true;
@@ -155,11 +447,11 @@ bool field_to_string(const char *field, std::string &string) {
 }
 
 // stub (should use #define'd 256-bit secret)
-void encrypt_packet(Packet const &plaintext, Packet &ciphertext) {
+void encrypt_packet(Packet &packet) {
 }
 
 // stub (should use #define'd 256-bit secret)
-void decrypt_packet(Packet const &ciphertext, Packet &plaintext) {
+void decrypt_packet(Packet &packet) {
 }
 
 // stub
--- test_protocol.cpp
+++ test_protocol.cpp
@@ -5,6 +5,167 @@
 #include <cassert>
 #include "protocol.h"
 
+void test_login_request() {
+    login_request_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    msg.username = "alice";
+    msg.card = "31415-926535";
+    msg.pin = "1234";
+
+    int rc;
+
+    Packet packet;
+    rc = encode_login_request(packet, msg);
+    assert(rc);
+
+    std::string username;
+    rc = get_str_field(packet, 3, username);
+    assert(rc);
+    assert(username == msg.username);
+
+    login_request_t dec;
+    rc = decode_login_request(packet, dec);
+    assert(rc);
+
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+    assert(msg.username == dec.username);
+    assert(msg.card == dec.card);
+    assert(msg.pin == dec.pin);
+}
+
+void test_balance_request() {
+    balance_request_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    msg.auth_token[3] = 's';
+
+    int rc;
+
+    Packet packet;
+    rc = encode_balance_request(packet, msg);
+    assert(rc);
+
+    balance_request_t dec;
+    rc = decode_balance_request(packet, dec);
+    assert(rc);
+
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.auth_token, dec.auth_token, FIELD_SIZE) == 0);
+}
+
+
+void test_withdraw_request() {
+    withdraw_request_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    msg.auth_token[3] = 's';
+    msg.amount = 12;
+
+    int rc;
+
+    Packet packet;
+    rc = encode_withdraw_request(packet, msg);
+    assert(rc);
+
+    withdraw_request_t dec;
+    rc = decode_withdraw_request(packet, dec);
+    assert(rc);
+
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.auth_token, dec.auth_token, FIELD_SIZE) == 0);
+    assert(msg.amount == dec.amount);
+}
+
+void test_transfer_request() {
+    transfer_request_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    msg.auth_token[3] = 's';
+    msg.amount = 12;
+    msg.destination = "zanzibar";
+    int rc;
+    Packet packet;
+    rc = encode_transfer_request(packet, msg);
+    assert(rc);
+    transfer_request_t dec;
+    rc = decode_transfer_request(packet, dec);
+    assert(rc);
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.auth_token, dec.auth_token, FIELD_SIZE) == 0);
+    assert(msg.amount == dec.amount);
+    assert(msg.destination == dec.destination);
+}
+
+void test_login_response() {
+    login_response_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    msg.auth_token[3] = 's';
+    int rc;
+    Packet packet;
+    rc = encode_login_response(packet, msg);
+    assert(rc);
+    login_response_t dec;
+    rc = decode_login_response(packet, dec);
+    assert(rc);
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.auth_token, dec.auth_token, FIELD_SIZE) == 0);
+}
+
+
+void test_balance_response() {
+    balance_response_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    msg.balance = 3;
+    int rc;
+    Packet packet;
+    rc = encode_balance_response(packet, msg);
+    assert(rc);
+    balance_response_t dec;
+    rc = decode_balance_response(packet, dec);
+    assert(rc);
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+    assert(msg.balance == dec.balance);
+}
+
+void test_withdraw_response() {
+    withdraw_response_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    int rc;
+    Packet packet;
+    rc = encode_withdraw_response(packet, msg);
+    assert(rc);
+    withdraw_response_t dec;
+    rc = decode_withdraw_response(packet, dec);
+    assert(rc);
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+}
+
+void test_transfer_response() {
+    transfer_response_t msg;
+    msg.atm_nonce[12] = 'z';
+    msg.bank_nonce[7] = 'f';
+    int rc;
+    Packet packet;
+    rc = encode_transfer_response(packet, msg);
+    assert(rc);
+    transfer_response_t dec;
+    rc = decode_transfer_response(packet, dec);
+    assert(rc);
+    assert(memcmp(msg.atm_nonce, dec.atm_nonce, FIELD_SIZE) == 0);
+    assert(memcmp(msg.bank_nonce, dec.bank_nonce, FIELD_SIZE) == 0);
+}
+
 void test_packet_access() {
     int rc;
     Packet packet;
@@ -15,6 +176,7 @@ void test_packet_access() {
     set_int_field(packet, 0, 15);
     set_str_field(packet, 1, "Hello, world");
     set_dat_field(packet, 2, data1);
+    set_int_field(packet, 3, 42);
 
     int i;
     rc = get_int_field(packet, 0, i);
@@ -30,6 +192,11 @@ void test_packet_access() {
     rc = get_dat_field(packet, 2, data2);
     assert(rc);
     assert(memcmp(data1, data2, FIELD_SIZE) == 0);
+
+    unsigned int j;
+    rc = get_unsigned_int_field(packet, 3, j);
+    assert(rc);
+    assert(j == 42);
 }
 
 void test_int_field() {
@@ -63,5 +230,14 @@ void test_int_field() {
 
 int main(int argc, const char *argv[]) {
     test_int_field();
+    test_packet_access();
+    test_login_request();
+    test_balance_request();
+    test_withdraw_request();
+    test_transfer_request();
+    test_login_response();
+    test_balance_response();
+    test_withdraw_response();
+    test_transfer_response();
     return 0;
 }
